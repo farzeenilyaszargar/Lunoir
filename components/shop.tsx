1763ff9a60b89698221f713902bdf02dfc5b7737
farzeenilyaszargar <farzeeilyaszargarg@gmail.com>
2025-08-16 04:55:11 +0530

    shop supdate

--- components/shop.tsx
+++ components/shop.tsx
@@ -20,14 +20,14 @@ function Stars({ rating = 5 }: { rating?: number }) {
 
 export default function ProductGrid() {
   const products = [
-    { id: 1, name: 'Rosy Posies', price: 799, color: 'Jet Blue', badge: 'Best Seller', image: '/images/shirts/1.jpeg' },
-    { id: 2, name: 'Flower Warrior', price: 899, color: 'Bone', badge: 'Limited', image: '/images/shirts/2.jpeg' },
-    { id: 3, name: 'Ocean and Life', price: 699, color: 'White', badge: 'New', image: '/images/shirts/3.jpeg' },
-    { id: 4, name: 'Poppies and Cherries', price: 849, color: 'Jet Blue', badge: 'Hot' , image: '/images/shirts/4.jpeg'},
-    { id: 5, name: 'Off White Floral', price: 799, color: 'Creamy White', badge: 'Best Seller', image: '/images/shirts/5.jpeg' },
-    { id: 6, name: 'Oh My Stars', price: 899, color: 'Bluey Blue', badge: 'Limited', image: '/images/shirts/6.jpeg' },
-    { id: 7, name: 'Basic Ah Hoodie', price: 699, color: 'Charcoal', badge: 'New', image: '/images/shirts/9.webp' },
-    { id: 8, name: 'Floral King', price: 849, color: 'Off White', badge: 'Hot' , image: '/images/shirts/8.jpeg'},
+    { id: 1, name: 'Rosy Posies', price: 799, color: 'Jet Blue', badge: 'Best Seller', image: '/images/shirts/1.jpeg', rating:4.3, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'],  },
+    { id: 2, name: 'Flower Warrior', price: 899, color: 'Bone', badge: 'Limited', image: '/images/shirts/2.jpeg', rating:3.9, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'],  },
+    { id: 3, name: 'Ocean and Life', price: 699, color: 'White', badge: 'New', image: '/images/shirts/3.jpeg' , rating:4.8, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'], },
+    { id: 4, name: 'Poppies and Cherries', price: 849, color: 'Jet Blue', badge: 'Hot' , image: '/images/shirts/4.jpeg', rating:4.5, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'], },
+    { id: 5, name: 'Off White Floral', price: 799, color: 'Creamy White', badge: 'Best Seller', image: '/images/shirts/5.jpeg', rating:4.7, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'],  },
+    { id: 6, name: 'Oh My Stars', price: 899, color: 'Bluey Blue', badge: 'Limited', image: '/images/shirts/6.jpeg', rating:4.2, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'],  },
+    { id: 7, name: 'Crazy Ah Hoodie', price: 699, color: 'Charcoal', badge: 'New', image: '/images/shirts/10.jpeg', rating:4.6, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'],  },
+    { id: 8, name: 'Floral King', price: 849, color: 'Off White', badge: 'Hot' , image: '/images/shirts/8.jpeg', rating:4.9, totalReviews: 103, sizes:['S', 'M', 'l', 'XL'], },
   ]
   return (
     <section id="grid" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 ">
@@ -43,23 +43,27 @@ export default function ProductGrid() {
   )
 }
 
-function ProductCard({name, price, color, badge, image}:{name:string; price:number; color:string; badge:string; image:string}){
+function ProductCard({name, price, color, badge, image, rating, totalReviews, sizes}:{name:string; price:number; color:string; badge:string; image:string, rating:number, totalReviews:number, sizes:string[]}){
   return (
     <a href="#" className="group rounded-2xl bg-white border border-zinc-200 overflow-hidden shadow-sm hover:shadow-md transition-shadow">
       <div className="relative aspect-square ">
-        <div className="absolute inset-0 grid place-items-center bg-zinc-100 overflow-hidden">
-            <Image src={image} alt={name} width={200} height={200} className="w-full items-center justify-center flex"/>
+        <div className="absolute inset-0 grid place-items-center bg-zinc-100 overflow-hidden ">
+            <Image src={image} alt={name} width={200} height={200} className="w-full"/>
         </div>
         <span className="absolute left-2 top-2 rounded-full bg-black px-2 py-1 text-[10px] font-medium text-white">{badge}</span>
       </div>
       <div className="p-3">
         <div className="flex items-center justify-between">
-          <h3 className="text-sm font-medium line-clamp-1">{name}</h3>
+          <h3 className="text-base font-medium line-clamp-1">{name}</h3>
           <span className="text-sm font-semibold">₹{price}</span>
         </div>
-        <p className="mt-1 text-xs text-zinc-500">{color}</p>
-        <div className="mt-2 flex items-center gap-1 text-[10px] text-zinc-600"><Stars rating={4}/> <span>1.2k</span></div>
-        <button className="mt-3 w-full rounded-xl bg-black py-2 text-white text-sm font-medium hover:bg-zinc-800 active:scale-[.99]">Add to cart</button>
+        <div className=" border-zinc-400 mt-2 rounded-2xl flex flex-row justify-center">
+          <button className="border p-0.5 rounded ml-2 pl-2 pr-2 text-[14px] hover:bg-black hover:text-white">S</button>
+          <button className="border p-0.5 rounded ml-2 pl-2 pr-2 text-[14px] hover:bg-black hover:text-white">M</button>
+          <button className="border p-0.5 rounded ml-2 pl-2 pr-2 text-[14px] hover:bg-black hover:text-white">L</button>
+          <button className="border p-0.5 rounded ml-2 pl-2 pr-2 text-[14px] hover:bg-black hover:text-white">XL</button>
+        </div>
+        <button className="mt-3 w-full border rounded-xl bg-black py-2 text-white text-sm font-medium hover:bg-white hover:text-black">Add to cart</button>
       </div>
     </a>
   )
