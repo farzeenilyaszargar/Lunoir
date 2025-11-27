34a6db41abb5ac6587f073197ccec1918d7fd395
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2025-11-27 14:40:56 +0530

    vercel adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
-import Banner from "@/components/Banner";
+import Banner from "@/components/HeroSection";
 import FAQ from "@/components/Faq";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Reviews from "@/components/Reviews";
 import Shop from "@/components/Shop";
-import Trust from "@/components/Trust";
 import Load from '@/components/CompLoading';
-import Shop2 from "@/components/Shop2";
 
 
 export default function Home() {
   return (
     <div >
-      <Load/>
-      <Header/>
-      <Banner/>
-      <Shop/>
-      {/* <Shop2/> */}
-      <Reviews/>
-      <FAQ/>
-      {/*<Trust/>*/}
-      <Footer/>
+      <Load />
+      <Header />
+      <Banner />
+      <Shop />
+      <Reviews />
+      <FAQ />
+      <Footer />
     </div>
   );
 }
