e75b56b8e09bd0f780a20ae06837ca187f1cbe69
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2025-11-27 18:46:00 +0530

    deelted supabase

--- app/api/supabase/route.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "@/lib/supabaseClient";
-
-export async function GET() {
-  const { data, error } = await client.from("users").select("*");
-
-  return Response.json({ data, error });
-}
